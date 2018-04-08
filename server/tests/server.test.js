0b343d28d0cb03c7267b4fb5d0adc89b4c414609
anisa07 <askany07@gmail.com>
2018-04-08 12:06:22 +0300

    get test home

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,16 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Home } = require('./../models/home');
 
+const homeTest = [{
+	text: 'First test'
+}, {
+	text: 'Second test'
+}];
+
 beforeEach((done) => {
-	Home.remove({}).then(() => done())
+	Home.remove({})
+		.then(() => Home.insertMany(homeTest))
+		.then(() => {done()})
 });
 
 describe('POST /home', () => {
@@ -24,10 +32,22 @@ describe('POST /home', () => {
 	    		return done(err);
 
 		    Home.find().then((todos) => {
-			    expect(todos.length).toBe(1);
-			    expect(todos[0].text).toBe(text);
+			    expect(todos.length).toBe(3);
+			    expect(todos[2].text).toEqual(text);
 			    done();
 		    }).catch((e) => done(e));
 	    })
   });
+});
+
+describe('GET /home', () => {
+	it('should return all home texts', (done) => {
+		request(app)
+			.get('/home')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.data.length).toBe(2);
+			})
+			.end(done);
+	})
 });
