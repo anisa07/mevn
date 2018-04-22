ed4e50cbb694bec87f5834d9e8c29de818ee0ed4
anisa07 <askany07@gmail.com>
2018-04-22 12:57:49 +0300

    pages routes

---
+++ server/routes/pages.routes.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-unused-expressions,prefer-destructuring,consistent-return,indent */
+const express = require('express');
+const _ = require('lodash');
+const { ObjectID } = require('mongodb');
+
+const { Home } = require('../models/home');
+const { authenticate } = require('../middleware/authenticate');
+
+const pageRouter = express.Router();
+
+const Pages = {
+	home: Home,
+};
+const urls = ['home', 'blog', 'awards', 'events', 'experience', 'mentorship', 'projects', 'contacts'];
+Object.freeze(Pages);
+Object.freeze(urls);
+const findUrl = originalUrl => urls.find(url => originalUrl.includes(url));
+
+pageRouter.get('/*', (req, res) => {
+	const url = findUrl(req.originalUrl);
+	if (url) {
+		const Model = Pages[url];
+		Model.find().then((data) => {
+			res.send({ data });
+		}, (e) => {
+			res.status(400).send(e);
+		});
+	} else {
+		res.status(404).send();
+	}
+});
+
+pageRouter.post('/*', authenticate, (req, res) => {
+	const url = findUrl(req.originalUrl);
+	if (url) {
+		const Model = Pages[url];
+		const model = new Model(req.body);
+		model.save().then((doc) => {
+			res.send(doc);
+		}, (e) => {
+			res.status(400).send(e);
+		});
+	} else {
+		res.status(404).send();
+	}
+});
+
+pageRouter.delete('/*/:id', authenticate, (req, res) => {
+	const url = findUrl(req.originalUrl);
+	if (url) {
+		const id = req.params.id;
+		const Model = Pages[url];
+		if (!ObjectID.isValid(id)) {
+			return res.status(404).send();
+		}
+		Model.findByIdAndRemove(id).then((data) => {
+			if (!data) {
+				res.status(404).send(data);
+			}
+			res.status(200).send({ data });
+		}).catch(e =>
+			res.status(400).send(e));
+	} else {
+		res.status(404).send();
+	}
+});
+
+pageRouter.patch('/*/:id', authenticate, (req, res) => {
+	const url = findUrl(req.originalUrl);
+	if (url) {
+		const id = req.params.id;
+		const Model = Pages[url];
+		const body = _.pick(req.body, _.keys(req.body));
+
+		if (!ObjectID.isValid(id)) {
+			return res.status(404).send();
+		}
+
+		Model.findByIdAndUpdate(id, { $set: body }, { new: true })
+			.then((data) => {
+				if (!data) {
+					res.status(404).send();
+				}
+				res.send({ data });
+			})
+			.catch((e) => {
+				res.status(400).send(e);
+			});
+	} else {
+		res.status(404).send();
+	}
+});
+
+module.exports = pageRouter;
--- server/server.js
+++ server/server.js
@@ -1,74 +1,25 @@
 /* eslint-disable indent,no-tabs,no-unused-expressions,
 no-sequences,consistent-return,prefer-destructuring */
 require('./config/config');
-const _ = require('lodash');
+// const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 // eslint-disable-next-line no-unused-vars
 const mongoose = require('./db/mongoose');
-const { ObjectID } = require('mongodb');
-
-const { Home } = require('./models/home');
-const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
+
 app.use('/users', require('./routes/user.routes'));
-
-app.post('/home', authenticate, (req, res) => {
-	const home = new Home({
-		text: req.body.text,
-	});
-	home.save().then((doc) => {
-		res.send(doc);
-	}, (e) => {
-		res.status(400).send(e);
-	});
-});
-
-app.get('/home', (req, res) => {
-	Home.find().then((data) => {
-		res.send({ data });
-	}), (e) => {
-		res.status(400).send(e);
-	};
-});
-
-app.delete('/home/:id', authenticate, (req, res) => {
-	const id = req.params.id;
-	if (!ObjectID.isValid(id)) {
-		return res.status(404).send();
-	}
-	Home.findByIdAndRemove(id).then((data) => {
-		if (!data) {
-			res.send(data);
-		} else {
-			res.status(400).send();
-		}
-	}).catch(e =>
-		res.status(400).send(e));
-});
-
-app.patch('/home/:id', authenticate, (req, res) => {
-	const id = req.params.id;
-	const body = _.pick(req.body, ['text']);
-
-	if (!ObjectID.isValid(id)) {
-		return res.status(404).send(e);
-	}
-
-	Home.findByIdAndUpdate(id, {$set: body}, {new: true})
-		.then((data) => {
-			if (!data) {
-				res.status(404).send();
-			}
-			res.send({ data });
-		}).catch((e) => {
-		res.status(400).send(e);
-	});
-});
+// const myLogger = function (req, res, next) {
+// 	console.log('req', req.originalUrl);
+// 	console.log('req', req.method);
+// 	next();
+// }
+// app.use(myLogger);
+app.use('/pages', require('./routes/pages.routes'));
 
 app.listen(port, () => {
 });
